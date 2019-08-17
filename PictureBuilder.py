6dc8a0b159858e9d5b4d28d98d0b957bf7ccaa8e
kitozawr <kitozawr07@mail.ru>
2019-08-17 19:42:32 +0300

    OpenFile

--- PictureBuilder.py
+++ PictureBuilder.py
@@ -3,3 +3,10 @@
 def do_hello(self, args):
     """Просто выводит 'hello world' на экран"""
     print ("hello world")
+
+def do_open_file(initdir="/", filetype="*.jpg;*.dat")
+    from tkinter import filedialog
+    from tkinter import *
+    root = Tk()
+    root.filename =  filedialog.askopenfilename(initialdir = initdir,title = "Select file",filetypes = (("Allowed types",filetype),("all files","*.*")))
+    return (root.filename)
