1ffb88c228a1cff28123ae80298e6a52a3dd113c
kitozawr <kitozawr07@mail.ru>
2019-08-21 18:07:12 +0300

    Поворот и сохранение графика. Исправлен баг с 2 осями

--- PictureBuilder.py
+++ PictureBuilder.py
@@ -9,16 +9,29 @@ import numpy as np
 
 array = np.array([[1,2,3,4], [5,6,7,8], [9,10,11,12]])
 sns.set()
-plot = sns.heatmap(array)
 if os.path.isfile('spectrograph_last_dir.pkl'):
     with open('spectrograph_last_dir.pkl','rb') as dir_save_file:
         initdir= pickle.load(dir_save_file)
+else:
+    with open('spectrograph_last_dir.pkl','wb') as dir_save_file:
+        pickle.dump('/', dir_save_file)
+
+def do_rotate(self, args):
+    """Вращает на 90 градусов против часовой"""
+    global array
+    array=np.rot90(array)
 
 def do_hello(self, args):
     """Просто выводит 'hello world' на экран"""
     print ("hello world")
 
-def do_ask_open_file(self, initdir="/"):
+def do_ask_save_file(self, args):
+    """Открытие GUI окна выбора файла для сохранения"""
+    global plot
+    file_name= filedialog.asksaveasfilename(filetypes=(("PNG files only","*.png"),("All files","*.*")))
+    plt.savefig(file_name)
+
+def do_ask_open_file(self, args):
     """Открытие GUI окна выбора файла для открытия"""
     root = Tk()
     root.filename =  filedialog.askopenfilename(initialdir = initdir,title = "Select file",filetypes=(("Data files only", "*.dat"),("PNG files only","*.png"),("All files","*.*")))
@@ -54,9 +67,6 @@ def do_plot (self, args):
 def do_exit (self, args):
     """Выход из работы. Альтернатива CTRL+c затем ENTER"""
     sys.exit()
-
-def do_echo (self, args):
-    print(args)
 
 def do_print_array (self,args):
     """Выводит массив в текстовом режиме"""
