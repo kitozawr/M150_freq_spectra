edf54abe4205ade85993fba605e7667bc6012d9a
kitozawr <kitozawr07@mail.ru>
2019-08-21 07:43:01 +0300

    Открыть новый файл предложет из последней папки

--- PictureBuilder.py
+++ PictureBuilder.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import filedialog
 import sys
 import os
+import pickle
 import matplotlib.pylab as plt
 import seaborn as sns
 import numpy as np
@@ -9,6 +10,9 @@ import numpy as np
 array = np.array([[1,2,3,4], [5,6,7,8], [9,10,11,12]])
 sns.set()
 plot = sns.heatmap(array)
+if os.path.isfile('spectrograph_last_dir.pkl'):
+    with open('spectrograph_last_dir.pkl','rb') as dir_save_file:
+        initdir= pickle.load(dir_save_file)
 
 def do_hello(self, args):
     """Просто выводит 'hello world' на экран"""
@@ -19,6 +23,9 @@ def do_ask_open_file(self, initdir="/"):
     root = Tk()
     root.filename =  filedialog.askopenfilename(initialdir = initdir,title = "Select file",filetypes=(("Data files only", "*.dat"),("PNG files only","*.png"),("All files","*.*")))
     filename_extension = os.path.splitext(root.filename)[-1]
+    directory= os.path.dirname(root.filename)
+    with open('spectrograph_last_dir.pkl','wb') as dir_save_file:
+        pickle.dump(directory, dir_save_file)
 
     if  filename_extension == ".png":
         do_image_to_array(self='', name_of_file=root.filename)
@@ -26,28 +33,31 @@ def do_ask_open_file(self, initdir="/"):
         do_data_to_array(self='', name_of_file=root.filename)
 
 def do_image_to_array(self, name_of_file):
+    """Открыть PNG: image_to_array <путь>"""
     global array
     print (name_of_file)
     array= plt.imread(name_of_file)
 
 def do_data_to_array(self, name_of_file):
+    """Открыть DAT: data_to_array <путь>"""
     global array
     array= np.fromfile(name_of_file, dtype='>i2')
     array= np.reshape(array[4:], (array[1],array[3]))
 
 def do_plot (self, args):
-    """Открывает окно с графиком по текущим настройкам в неблокирующем режиме"""
+    """Открывает окно с графиком и текущими настройками в неблокирующем режиме"""
     global plot
     plot = sns.heatmap(array, cmap="nipy_spectral")
     plt.ion()
     plt.show()
 
 def do_exit (self, args):
-    """Выход из работы. Альтернатив CTRL+c затем ENTER"""
+    """Выход из работы. Альтернатива CTRL+c затем ENTER"""
     sys.exit()
 
 def do_echo (self, args):
     print(args)
 
 def do_print_array (self,args):
+    """Выводит массив в текстовом режиме"""
     print (array)
